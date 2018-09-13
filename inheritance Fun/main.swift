c5ae1d7f927cd819e5fc4fb51acbb3585e2dc541
Reid, Jon Carl <jreid2@hk223-27731.local>
2018-09-13 10:11:57 -0700

    Initial Commit

---
+++ inheritance Fun/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  inheritance Fun
+//
+//  Created by Reid, Jon Carl on 9/13/18.
+//  Copyright © 2018 Reid, Jon Carl. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
